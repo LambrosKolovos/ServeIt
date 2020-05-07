aaaa044757e09b5c27e3f2df18e6e798da08aeba
Eirini Papadopoulou <62512626+eirinisof10@users.noreply.github.com>
2020-05-07 21:08:01 +0300

    Update item_notes.java

--- app/src/main/java/com/example/serveIt/employee_activities/item_notes.java
+++ app/src/main/java/com/example/serveIt/employee_activities/item_notes.java
@@ -1,13 +1,21 @@
 package com.example.serveIt.employee_activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.serveIt.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class item_notes extends AppCompatActivity {
+    int counter = 1;
+    String totalCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +24,64 @@ public class item_notes extends AppCompatActivity {
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
         bottomNav.setSelectedItemId(R.id.new_order);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.store:
+                        startActivity(new Intent(getApplicationContext(), store_layout.class));
+                        overridePendingTransition(0,0);
+                    case R.id.new_order:
+                        return true;
+                    case R.id.active_orders:
+                        startActivity(new Intent(getApplicationContext(), active_order.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.settings:
+                        startActivity(new Intent(getApplicationContext(), settings.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
 
+        final Button incBtn = findViewById(R.id.incBtn);
+        final Button decBtn = findViewById(R.id.decBtn);
+        final Button addBtn = findViewById(R.id.addBtn);
+        final TextView quantity = findViewById(R.id.quantity);
 
+        incBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                totalCounter = Integer.toString(counter);
+                quantity.setText(totalCounter);
+            }
+        });
+        decBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(counter > 1) {
+                    counter--;
+                    totalCounter = Integer.toString(counter);
+                    quantity.setText(totalCounter);
+                }
+                else{
+                    quantity.setText("1");
+                }
+            }
+        });
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), new_order.class));
+            }
+        });
+    }
+
+    public void onBackPressed() {
+        startActivity(new Intent(getApplicationContext(), new_order.class));
     }
 }
+
