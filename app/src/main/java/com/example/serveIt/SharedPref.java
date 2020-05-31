ecdbc011bcc3d4d543cbc37d562c9fd373a36890
LambrosKolovos <kolovosl@csd.auth.gr>
2020-06-01 01:36:42 +0300

    Added missing files

---
+++ app/src/main/java/com/example/serveIt/SharedPref.java
@@ -0,0 +1,24 @@
+package com.example.serveIt;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPref {
+    SharedPreferences mySharePref;
+
+    public SharedPref(Context context) {
+        mySharePref = context.getSharedPreferences("serveIt", Context.MODE_PRIVATE);
+    }
+
+    public void setNightMode(Boolean state) {
+        SharedPreferences.Editor editor = mySharePref.edit();
+        editor.putBoolean("nightmode", state);
+        editor.commit();
+    }
+
+    public Boolean loadNightMode() {
+        Boolean state = mySharePref.getBoolean("nightmode", false);
+        return state;
+    }
+
+}
